b36bebf897e417c670d6edaf4c84871eea2849d3
morenice <hyoungguyo@hotmail.com>
2020-06-25 14:50:24 +0900

    Python dictionary technics

--- language/python/dictionary.py
+++ language/python/dictionary.py
@@ -1,13 +1,26 @@
 if __name__ == "__main__":
     # Create dictionary with three entries.
-    values = {'a' : 1, 'b' : 2, 'c' : 3}
+    values = {'a': 1, 'b': 2, 'c': 3}
 
-    # Raise exception
-    #print(values['d'])
+    # raise exception
+    try:
+        print(values['d'])
+    except KeyError as e:
+        print(f'Error: {e}')
 
-    # Avoid KeyError exception
+    # avoid KeyError exception
     # return None
     print(values.get('d'))
 
     # get 'a'
     print(values.get('a'))
+
+    # not using exception technic
+    values2 = {'a': [], 'b': [1,2,3]}
+    for i in values2.get('c', []):
+        print(i)
+
+    # comprehension
+    new_value = {i: str(i) for i in values2.get('b', [])}
+    print(new_value)
+
